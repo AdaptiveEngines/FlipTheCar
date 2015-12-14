93aa61aa0b65795c939a25f4b866aba13bb430c4
verma088 <verma.arvind1993@gmail.com>
2015-12-14 14:17:21 -0500

    working with the mouse click ryt now

---
+++ Assets/BounceBack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("worked");
+        //other.transform.position = Vector3.Reflect(other.transform.position, Vector3.right);
+    }
+}
